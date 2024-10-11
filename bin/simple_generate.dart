9da8efbe8f12c345693783a9b5abc5bf4457171e
jinhankim <jinhan38@naver.com>
2024-10-11 15:34:29 +0900

    line_space.dart 추가, text_util.dart 수정

--- bin/simple_generate.dart
+++ bin/simple_generate.dart
@@ -53,6 +53,7 @@ void createWidgets() {
   createFile("lib/widgets/custom_gradient_text.dart", customGradientText);
   createFile("lib/widgets/my_divider.dart", myDivider);
   createFile("lib/widgets/image_widget.dart", imageWidget);
+  createFile("lib/widgets/line_space.dart", lineSpace);
 }
 
 void createDialog() {
@@ -316,25 +317,211 @@ var textUtil = '''
 import 'package:flutter/material.dart';
 
 class TextUtil {
-  static Widget text(
+  static Widget get17Bold(
+    BuildContext context,
     String text, {
-    required TextStyle style,
+    TextStyle? style,
     TextAlign? textAlign,
+    Color color = Colors.black,
+    int? maxLines,
+    bool softWrap = true,
+    TextOverflow? overflow,
+    FontWeight fontWeight = FontWeight.bold,
   }) {
-    return Text(text, style: style, textAlign: textAlign);
+    return Text(
+      text,
+      style: style ?? getTitleLargeStyle(context, color, fontWeight),
+      textAlign: textAlign,
+      maxLines: maxLines,
+      softWrap: softWrap,
+      overflow: overflow,
+      textHeightBehavior: const TextHeightBehavior(applyHeightToLastDescent: false),
+    );
   }
 
-  /// fontSize = 11
-  static TextStyle get11(BuildContext context, Color color) {
-    return Theme.of(context).textTheme.labelSmall!.copyWith(color: color);
+  static Widget get14w600(
+    BuildContext context,
+    String text, {
+    TextStyle? style,
+    TextAlign? textAlign,
+    Color color = Colors.black,
+    int? maxLines,
+    bool softWrap = true,
+    TextOverflow? overflow,
+    FontWeight fontWeight = FontWeight.w600,
+  }) {
+    return Text(
+      text,
+      style: style ?? getTitleMediumStyle(context, color, fontWeight),
+      textAlign: textAlign,
+      maxLines: maxLines,
+      softWrap: softWrap,
+      overflow: overflow,
+      textHeightBehavior: const TextHeightBehavior(applyHeightToLastDescent: false),
+    );
+  }
+
+  static Widget get12w500(
+    BuildContext context,
+    String text, {
+    TextStyle? style,
+    TextAlign? textAlign,
+    Color color = Colors.black,
+    int? maxLines,
+    bool softWrap = true,
+    TextOverflow? overflow,
+    FontWeight fontWeight = FontWeight.w500,
+  }) {
+    return Text(
+      text,
+      style: style ?? getTitleSmallStyle(context, color, fontWeight),
+      textAlign: textAlign,
+      maxLines: maxLines,
+      softWrap: softWrap,
+      overflow: overflow,
+      textHeightBehavior: const TextHeightBehavior(applyHeightToLastDescent: false),
+    );
+  }
+
+  static Widget get12Normal(
+    BuildContext context,
+    String text, {
+    TextStyle? style,
+    TextAlign? textAlign,
+    Color color = Colors.black,
+    int? maxLines,
+    bool softWrap = true,
+    TextOverflow? overflow,
+    FontWeight fontWeight = FontWeight.normal,
+  }) {
+    return Text(
+      text,
+      style: style ?? getBodyLargeStyle(context, color, fontWeight),
+      textAlign: textAlign,
+      maxLines: maxLines,
+      softWrap: softWrap,
+      overflow: overflow,
+      textHeightBehavior: const TextHeightBehavior(applyHeightToLastDescent: false),
+    );
+  }
+
+  static Widget get10w500(
+    BuildContext context,
+    String text, {
+    TextStyle? style,
+    TextAlign? textAlign,
+    Color color = Colors.black,
+    int? maxLines,
+    bool softWrap = true,
+    TextOverflow? overflow,
+    FontWeight fontWeight = FontWeight.w500,
+  }) {
+    return Text(
+      text,
+      style: style ?? getBodyMediumStyle(context, color, fontWeight),
+      textAlign: textAlign,
+      maxLines: maxLines,
+      softWrap: softWrap,
+      overflow: overflow,
+      textHeightBehavior: const TextHeightBehavior(applyHeightToLastDescent: false),
+    );
+  }
+
+  static Widget get9w500(
+    BuildContext context,
+    String text, {
+    TextStyle? style,
+    TextAlign? textAlign,
+    Color color = Colors.black,
+    int? maxLines,
+    bool softWrap = true,
+    TextOverflow? overflow,
+    FontWeight fontWeight = FontWeight.normal,
+  }) {
+    return Text(
+      text,
+      style: style ?? getBodySmallStyle(context, color, fontWeight),
+      textAlign: textAlign,
+      maxLines: maxLines,
+      softWrap: softWrap,
+      overflow: overflow,
+      textHeightBehavior: const TextHeightBehavior(applyHeightToLastDescent: false),
+    );
+  }
+
+  /// fontSize = 17
+  static TextStyle getTitleLargeStyle(BuildContext context, Color color, FontWeight fontWeight) {
+    return Theme.of(context).textTheme.titleLarge!.copyWith(color: color, fontWeight: fontWeight);
+  }
+
+  /// fontSize = 14
+  static TextStyle getTitleMediumStyle(BuildContext context, Color color, FontWeight fontWeight) {
+    return Theme.of(context).textTheme.titleMedium!.copyWith(color: color, fontWeight: fontWeight);
+  }
+
+  /// fontSize = 12
+  static TextStyle getTitleSmallStyle(BuildContext context, Color color, FontWeight fontWeight) {
+    return Theme.of(context).textTheme.titleSmall!.copyWith(color: color, fontWeight: fontWeight);
+  }
+
+  /// fontSize = 12
+  static TextStyle getBodyLargeStyle(BuildContext context, Color color, FontWeight fontWeight) {
+    return Theme.of(context).textTheme.bodyLarge!.copyWith(color: color, fontWeight: fontWeight);
+  }
+
+  /// fontSize = 10
+  static TextStyle getBodyMediumStyle(BuildContext context, Color color, FontWeight fontWeight) {
+    return Theme.of(context).textTheme.bodyMedium!.copyWith(color: color, fontWeight: fontWeight);
+  }
+
+  /// fontSize = 9
+  static TextStyle getBodySmallStyle(BuildContext context, Color color, FontWeight fontWeight) {
+    return Theme.of(context).textTheme.bodySmall!.copyWith(color: color, fontWeight: fontWeight);
   }
 
   static TextTheme setTextTheme() {
     return const TextTheme(
-      labelSmall: TextStyle(
-        fontSize: 11,
+      titleLarge: TextStyle(
+        fontSize: 17,
+        fontWeight: FontWeight.bold,
+        letterSpacing: -0.05,
+        color: Colors.black,
+        height: 1.1,
+      ),
+      titleMedium: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w600,
+        letterSpacing: -0.05,
+        color: Colors.black,
+        height: 1.1,
+      ),
+      titleSmall: TextStyle(
+        fontSize: 12,
         fontWeight: FontWeight.w500,
         color: Colors.black,
+        letterSpacing: -0.05,
+        height: 1.1,
+      ),
+      bodyLarge: TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.normal,
+        color: Colors.black,
+        letterSpacing: -0.05,
+        height: 1.1,
+      ),
+      bodyMedium: TextStyle(
+        fontSize: 10,
+        fontWeight: FontWeight.w500,
+        color: Colors.black,
+        letterSpacing: -0.05,
+        height: 1.1,
+      ),
+      bodySmall: TextStyle(
+        fontSize: 9,
+        fontWeight: FontWeight.w500,
+        color: Colors.black,
+        letterSpacing: -0.05,
+        height: 1.1,
       ),
     );
   }
@@ -964,7 +1151,6 @@ class KeyboardUtil {
 }
 ''';
 
-
 var loading = '''
 import 'package:flutter/material.dart';
 
@@ -1543,4 +1729,23 @@ class ImageWidget {
     );
   }
 }
+''';
+
+var lineSpace = '''
+import 'package:flutter/material.dart';
+
+class LineSpace {
+  static double get space8 => 8;
+
+  static double get space12 => 12;
+
+  static double get horizontal => 16;
+
+  static Widget box8() => SizedBox(height: LineSpace.space8);
+
+  static Widget box12() => SizedBox(height: LineSpace.space12);
+
+  static Widget boxHorizontal() => SizedBox(height: LineSpace.horizontal);
+}
+
 ''';
